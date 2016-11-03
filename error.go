ba371ec619810c8fb4769cdc43c343e5e55f3e9e
Max Cherry <mxmCherry@gmail.com>
2016-11-03 21:14:28 +0200

    Fixed Error.CaptchaImg JSON tag, added godoc comments for Error

--- error.go
+++ error.go
@@ -2,13 +2,17 @@ package vkapi
 
 import "fmt"
 
+// Error represents vk.com API error: https://vk.com/dev/errors
 type Error struct {
-	ErrorCode  uint64 `json:"error_code"`
-	ErrorMsg   string `json:"error_msg"`
+	ErrorCode uint64 `json:"error_code"`
+	ErrorMsg  string `json:"error_msg"`
+
+	// https://vk.com/dev/captcha_error
 	CaptchaSID string `json:"captcha_sid"`
-	CaptchaImg string `json:"captcha_sid"`
+	CaptchaImg string `json:"captcha_img"`
 }
 
+// Error returns human-readable error description.
 func (err Error) Error() string {
 	if err.CaptchaSID != "" {
 		return fmt.Sprintf("vkapi: %s (%d); captcha_sid: %s, captcha_img: %s",
