1e7618d172c151888127d913cd8404712cf323ef
Max Cherry <mxmCherry@gmail.com>
2016-11-05 21:27:28 +0200

    More reflect magic for ToParams (now safe even for pointer abuse)

--- to_params.go
+++ to_params.go
@@ -9,10 +9,18 @@ import (
 
 // ToParams builds request params from struct.
 // It returns nil params unless arg is not a struct.
-// Param names are detected from "json" struct tags.
+//
+// Param names are detected from "json" struct tags,
+// "-" tag (omit field) and "omitempty" modifier are respected.
+//
+// Nil values are always omited (even without "omitempty").
+// Chan, func, interface, map and complex values are also omited.
+//
 // Slices are serialized as comma-separated strings.
+//
+// Bools are serialized as 1 (true) and 0 (false).
 func ToParams(d interface{}) url.Values {
-	rv := reflect.ValueOf(d)
+	rv := dereference(reflect.ValueOf(d))
 	rt := rv.Type()
 	if rt.Kind() != reflect.Struct {
 		return nil
@@ -21,15 +29,38 @@ func ToParams(d interface{}) url.Values {
 	q := url.Values{}
 	for i, nf := 0, rt.NumField(); i < nf; i++ {
 		f := rt.Field(i)
-		v := rv.Field(i)
+		v := dereference(rv.Field(i))
+		vk := v.Kind()
+
+		// omit unsupported (unserializable) params:
+		switch vk {
+		case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map,
+			reflect.Uintptr, reflect.UnsafePointer,
+			reflect.Complex64, reflect.Complex128:
+			continue
+		}
+
+		// omit nil slices:
+		if vk == reflect.Slice && v.IsNil() {
+			continue
+		}
+
+		// omit invalid params (value of nil pointer etc):
+		if !v.IsValid() {
+			continue
+		}
+
 		vi := v.Interface()
 
-		name := strings.ToLower(f.Name)
+		name := ""
 		omitempty := false
 
 		tag := strings.Split(f.Tag.Get("json"), ",")
 		if n := len(tag); n >= 1 {
 			name = tag[0]
+			if name == "-" {
+				continue
+			}
 			if n >= 2 {
 				for _, s := range tag {
 					if s == "omitempty" {
@@ -38,25 +69,70 @@ func ToParams(d interface{}) url.Values {
 					}
 				}
 			}
+		} else {
+			name = strings.ToLower(f.Name)
 		}
 
-		if omitempty && reflect.DeepEqual(vi, reflect.Zero(f.Type).Interface()) {
+		if omitempty && isEmpty(vi) {
 			continue
 		}
 
-		if b, ok := vi.([]byte); ok {
-			vi = string(b)
-		} else if f.Type.Kind() == reflect.Slice {
-			n := v.Len()
-			ss := make([]string, n)
-			for j := 0; j < n; j++ {
-				ss[j] = fmt.Sprintf("%v", v.Index(j).Interface())
-			}
-			vi = strings.Join(ss, ",")
-		}
-
-		q.Set(name, fmt.Sprintf("%v", vi))
+		q.Set(name, toString(vi))
 	}
 
 	return q
+}
+
+// dereference dereferences pointer chain.
+func dereference(rv reflect.Value) reflect.Value {
+	for rv.Kind() == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	}
+	return rv
+}
+
+// toString stringifies value.
+func toString(v interface{}) string {
+	if v == nil {
+		return ""
+	}
+
+	switch x := v.(type) {
+
+	case bool:
+		if x {
+			return "1"
+		}
+		return "0"
+
+	case []byte:
+		return string(x)
+
+	default:
+		if rv := reflect.ValueOf(v); rv.Kind() == reflect.Slice {
+			n := rv.Len()
+			ss := make([]string, n)
+			for j := 0; j < n; j++ {
+				ss[j] = toString(dereference(rv.Index(j)).Interface())
+			}
+			return strings.Join(ss, ",")
+		}
+		return fmt.Sprintf("%v", v)
+
+	}
+}
+
+// isEmpty tells if value is empty.
+func isEmpty(v interface{}) bool {
+	rv := reflect.ValueOf(v)
+	if !rv.IsValid() {
+		return true
+	}
+
+	zero := reflect.Zero(rv.Type())
+	if !zero.IsValid() {
+		return false
+	}
+
+	return reflect.DeepEqual(v, zero.Interface())
 }
--- to_params_test.go
+++ to_params_test.go
@@ -11,21 +11,32 @@ import (
 
 var _ = Describe("ToParams", func() {
 	It("should build params from struct", func() {
+		s := "string"
 		d := struct {
+			Bool     bool     `json:"bool_param"`
 			String   string   `json:"string_param"`
 			Number   uint64   `json:"number_param"`
 			Repeated []uint64 `json:"repeated_param"`
-			Empty    string   `json:"empty,omitempty"`
+			Empty    string   `json:"empty_param,omitempty"`
+			Omitted  string   `json:"-"`
+			Pointer  *string  `json:"pointer_param"`
+			Nil      *string  `json:"nil_param"`
 		}{
+			Bool:     true,
 			String:   "string_value",
 			Number:   42,
 			Repeated: []uint64{1, 2, 3},
 			Empty:    "",
+			Omitted:  "omitted",
+			Pointer:  &s,
+			Nil:      nil,
 		}
 		Expect(vkapi.ToParams(d)).To(Equal(url.Values{
+			"bool_param":     []string{"1"},
 			"string_param":   []string{"string_value"},
 			"number_param":   []string{"42"},
 			"repeated_param": []string{"1,2,3"},
+			"pointer_param":  []string{"string"},
 		}))
 	})
 })
